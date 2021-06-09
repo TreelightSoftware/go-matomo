3b3dd827e7be9d3619133d148eb9f1830dca15a4
Kevin Eaton <kevinhowardeaton@gmail.com>
2021-06-09 13:58:21 -0400

    initial user and event params

--- parameters.go
+++ parameters.go
@@ -1,5 +1,7 @@
 package matomo
 
+// field descriptions are all from the Matomo docs as of 20210609: https://developer.matomo.org/api-reference/tracking-api
+
 import (
 	"fmt"
 	"math/rand"
@@ -9,7 +11,8 @@ import (
 
 // Parameters are the content that gets sent to the API. If the field is nil, it is skipped. If it isn't nil, it will be
 // automatically encoded and added to the body of the request. sendImage will be set to false. We have a matomo tag, but
-// aren't currently using it and just using hard coded values.
+// aren't currently using it and just using hard coded values. Keep in mind that many of these fields are included for
+// completeness sake and will not likely be known or relevant in a server-side context (eg: the user's resolution).
 type Parameters struct {
 	RecommendedParameters     *RecommendedParameters
 	UserParameters            *UserParameters
@@ -22,32 +25,54 @@ type Parameters struct {
 
 // RecommendedParameters are the recommended parameters that really should be provided on each call if available
 type RecommendedParameters struct {
+	// The title of the action being tracked. It is possible to use slashes / to set one or several categories for this action. For example, Help / Feedback will create the Action Feedback in the category Help.
 	ActionName *string `json:"action_name" matomo:"action_name"`
-	URL        *string `json:"url" matomo:"url"`
-	UserID     *string `json:"_id" matomo:"_id"`
-	Rand       *int64  `json:"rand" matomo:"rand"` // generated at call time if not provided
-	APIV       *int64  `json:"apiv" matomo:"apiv"` // always set to 1
+	// The full URL for the current action.
+	URL *string `json:"url" matomo:"url"`
+	// The unique visitor ID, must be a 16 characters hexadecimal string. Every unique visitor must be assigned a different ID and this ID must not change after it is assigned. If this value is not set Matomo (formerly Piwik) will still track visits, but the unique visitors metric might be less accurate.
+	VisitorID *string `json:"_id" matomo:"_id"`
+	// Meant to hold a random value that is generated before each request. Using it helps avoid the tracking request being cached by the browser or a proxy. If not set, the SDK will set it for you.
+	Rand *int64 `json:"rand" matomo:"rand"` // generated at call time if not provided
+	// The parameter &apiv=1 defines the api version to use (currently always set to 1). The SDK sets this for you.
+	APIV *int64 `json:"apiv" matomo:"apiv"` // always set to 1
 }
 
 // UserParameters are user specific parameters for the event
 type UserParameters struct {
-	URLRef           *string      `json:"urlref" matomo:"fla"`
-	CVar             *string      `json:"_cvar" matomo:"_cvar"`
-	IDVC             *int64       `json:"_idvc" matomo:"_idvc"`
-	ViewTS           *int64       `json:"_viewts" matomo:"_viewts"`
-	IDTS             *int64       `json:"_idts" matomo:"_idts"`
-	CampaignName     *string      `json:"_rcn" matomo:"_rcn"`
-	CampaignKeyword  *string      `json:"_rck" matomo:"_rck"`
-	Resolution       *string      `json:"res" matomo:"res"`
-	CurrentHour      *string      `json:"h" matomo:"h"`
-	CurrentMinute    *string      `json:"m" matomo:"m"`
-	CurrentSecond    *string      `json:"s" matomo:"s"`
-	UserPlugins      *UserPlugins `json:"plugins" matomo:"-"`
-	CookiesSupported *bool        `json:"cookie" matomo:"cookie"`
-	UserAgent        *string      `json:"ua" matomo:"ua"`
-	Lang             *string      `json:"lang" matomo:"lang"`
-	UID              *string      `json:"uid" matomo:"uid"`
-	NewVisit         *bool        `json:"new_visit" matomo:"new_visit"`
+	// The full HTTP Referrer URL. This value is used to determine how someone got to your website (ie, through a website, search engine or campaign).
+	URLRef *string `json:"urlref" matomo:"urlref"`
+	//  Visit scope custom variables. This is a JSON encoded string of the custom variable array.
+	CVar *string `json:"_cvar" matomo:"_cvar"`
+	// The current count of visits for this visitor. To set this value correctly, it would be required to store the value for each visitor in your application (using sessions or persisting in a database). Then you would manually increment the counts by one on each new visit or "session", depending on how you choose to define a visit. This value is used to populate the report Visitors > Engagement > Visits by visit number.
+	IDVC *int64 `json:"_idvc" matomo:"_idvc"`
+	// The UNIX timestamp of this visitor's previous visit. This parameter is used to populate the report Visitors > Engagement > Visits by days since last visit.
+	ViewTS *int64 `json:"_viewts" matomo:"_viewts"`
+	// The UNIX timestamp of this visitor's first visit. This could be set to the date where the user first started using your software/app, or when he/she created an account. This parameter is used to populate the Goals > Days to Conversion report.
+	IDTS *int64 `json:"_idts" matomo:"_idts"`
+	// The Campaign name. Used to populate the Referrers > Campaigns report. Note: this parameter will only be used for the first pageview of a visit.
+	CampaignName *string `json:"_rcn" matomo:"_rcn"`
+	// The Campaign Keyword. Used to populate the Referrers > Campaigns report (clicking on a campaign loads all keywords for this campaign). Note: this parameter will only be used for the first pageview of a visit.
+	CampaignKeyword *string `json:"_rck" matomo:"_rck"`
+	//  The resolution of the device the visitor is using, eg 1280x1024.
+	Resolution *string `json:"res" matomo:"res"`
+	// The current hour (local time). The SDK will automatically set this if you don't.
+	CurrentHour *string `json:"h" matomo:"h"`
+	// The current minute (local time). The SDK will automatically set this if you don't.
+	CurrentMinute *string `json:"m" matomo:"m"`
+	// The current second (local time). The SDK will automatically set this if you don't.
+	CurrentSecond *string `json:"s" matomo:"s"`
+	// Various user plugins that the server likely won't know about.
+	UserPlugins *UserPlugins `json:"plugins" matomo:"-"`
+	// When set to 1, the visitor's client is known to support cookies.
+	CookiesSupported *bool `json:"cookie" matomo:"cookie"`
+	// An override value for the User-Agent HTTP header field. The user agent is used to detect the operating system and browser used.
+	UserAgent *string `json:"ua" matomo:"ua"`
+	// An override value for the Accept-Language HTTP header field. This value is used to detect the visitor's country if GeoIP is not enabled.
+	Lang *string `json:"lang" matomo:"lang"`
+	// Defines the User ID for this request. User ID is any non-empty unique string identifying the user (such as an email address or an username). To access this value, users must be logged-in in your system so you can fetch this user ID from your system, and pass it to Matomo. The User ID appears in the visits log, the Visitor profile, and you can Segment reports for one or several User ID (userId segment). When specified, the User ID will be "enforced". This means that if there is no recent visit with this User ID, a new one will be created. If a visit is found in the last 30 minutes with your specified User ID, then the new action will be recorded to this existing visit.
+	UserID *string `json:"uid" matomo:"uid"`
+	// If set to 1, will force a new visit to be created for this action. T
+	NewVisit *bool `json:"new_visit" matomo:"new_visit"`
 }
 
 // UserPlugins is a sub-struct of capabilities for a user
@@ -68,7 +93,16 @@ type ActionParameters struct {
 type PagePerformanceParameters struct {
 }
 
+// EventTrackParameters add context to a user's actions on your platform.
 type EventTrackingParameters struct {
+	// The event category. Must not be empty. (eg. Videos, Music, Games...)
+	Category *string `json:"e_c" matomo:"e_c"`
+	// The event action. Must not be empty. (eg. Play, Pause, Duration, Add Playlist, Downloaded, Clicked...)
+	Action *string `json:"e_a" matomo:"e_a"`
+	// The event name. (eg. a Movie name, or Song name, or File name...)
+	Name *string `json:"e_n" matomo:"e_n"`
+	// The event value. Must be a float or integer value (numeric), not a string.
+	Value *float64 `json:"e_v" matomo:"e_v"`
 }
 
 type ContentTrackingParameters struct {
@@ -77,18 +111,23 @@ type ContentTrackingParameters struct {
 type EcommerceParameters struct {
 }
 
-// stringPtr converts a static string to a pointer for use in the api
-func stringPtr(input string) *string {
+// StringPtr converts a static string to a pointer for use in the api
+func StringPtr(input string) *string {
 	return &input
 }
 
-// int64Ptr converts a static int64 to a pointer for use in the api
-func int64Ptr(input int64) *int64 {
+// Int64Ptr converts a static int64 to a pointer for use in the api
+func Int64Ptr(input int64) *int64 {
 	return &input
 }
 
-// boolPtr converts a static bool to a pointer for use in the api
-func boolPtr(input bool) *bool {
+// BoolPtr converts a static bool to a pointer for use in the api
+func BoolPtr(input bool) *bool {
+	return &input
+}
+
+// Float64Ptr converts a static float to a pointer for use in the api
+func Float64Ptr(input float64) *float64 {
 	return &input
 }
 
@@ -108,6 +147,18 @@ func (params *Parameters) encode() map[string]string {
 			ret[k] = v
 		}
 	}
+	if params.UserParameters != nil {
+		subRet := params.UserParameters.encode()
+		for k, v := range subRet {
+			ret[k] = v
+		}
+	}
+	if params.EventTrackingParameters != nil {
+		subRet := params.EventTrackingParameters.encode()
+		for k, v := range subRet {
+			ret[k] = v
+		}
+	}
 
 	return ret
 }
@@ -118,9 +169,9 @@ func (params *RecommendedParameters) encode() map[string]string {
 		return ret
 	}
 	// set the required constants
-	params.APIV = int64Ptr(1)
+	params.APIV = Int64Ptr(1)
 	if params.Rand == nil {
-		params.Rand = int64Ptr(rand.Int63n(99999999999999999))
+		params.Rand = Int64Ptr(rand.Int63n(99999999999999999))
 	}
 	// loop through the fields
 	ret["apiv"] = url.QueryEscape(fmt.Sprintf("%v", *params.APIV))
@@ -128,8 +179,8 @@ func (params *RecommendedParameters) encode() map[string]string {
 	if params.ActionName != nil {
 		ret["action_name"] = url.QueryEscape(*params.ActionName)
 	}
-	if params.UserID != nil {
-		ret["_id"] = url.QueryEscape(*params.UserID)
+	if params.VisitorID != nil {
+		ret["_id"] = url.QueryEscape(*params.VisitorID)
 	}
 	if params.URL != nil {
 		ret["url"] = url.QueryEscape(*params.URL)
@@ -144,7 +195,7 @@ func (params *UserParameters) encode() map[string]string {
 		return ret
 	}
 	now := time.Now()
-	// conver tthe fields
+	// convert the fields
 	if params.URLRef != nil {
 		ret["urlref"] = url.QueryEscape(*params.URLRef)
 	}
@@ -194,8 +245,8 @@ func (params *UserParameters) encode() map[string]string {
 	if params.Lang != nil {
 		ret["lang"] = url.QueryEscape(*params.Lang)
 	}
-	if params.UID != nil {
-		ret["uid"] = url.QueryEscape(*params.UID)
+	if params.UserID != nil {
+		ret["uid"] = url.QueryEscape(*params.UserID)
 	}
 	if params.NewVisit != nil {
 		ret["new_visit"] = url.QueryEscape("1")
@@ -277,6 +328,26 @@ func (params *UserPlugins) encode() map[string]string {
 		} else {
 			ret["ag"] = url.QueryEscape("0")
 		}
+	}
+
+	return ret
+}
+
+func (params *EventTrackingParameters) encode() map[string]string {
+	ret := map[string]string{}
+	if params == nil {
+		return ret
+	}
+	// both Action and Category are required
+	if params.Action != nil && params.Category != nil {
+		ret["e_c"] = url.QueryEscape(*params.Category)
+		ret["e_a"] = url.QueryEscape(*params.Action)
+	}
+	if params.Name != nil {
+		ret["e_n"] = url.QueryEscape(*params.Name)
+	}
+	if params.Value != nil {
+		ret["e_v"] = url.QueryEscape(fmt.Sprintf("%v", *params.Value))
 	}
 
 	return ret
--- parameters_test.go
+++ parameters_test.go
@@ -1,19 +1,22 @@
 package matomo
 
 import (
+	"fmt"
 	"math/rand"
+	"net/url"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEncoding(t *testing.T) {
+func TestAllEncoding(t *testing.T) {
 	Setup()
 	// start with just the recommended parameters
 	params := Parameters{
 		RecommendedParameters: &RecommendedParameters{
-			ActionName: stringPtr("test_action"),
-			Rand:       int64Ptr(rand.Int63n(9999999999999999)),
+			ActionName: StringPtr("test_action"),
+			Rand:       Int64Ptr(rand.Int63n(9999999999999999)),
 		},
 	}
 
@@ -23,4 +26,99 @@ func TestEncoding(t *testing.T) {
 	assert.NotZero(t, encoded["rand"])
 	assert.Equal(t, *params.RecommendedParameters.ActionName, encoded["action_name"])
 	assert.Equal(t, "1", encoded["apiv"])
+	// make sure a few other params don't exist
+	assert.Equal(t, 3, len(encoded)) // increment as other auto-populated fields are added
+	assert.Empty(t, encoded["_rck"])
+
+	encoded = allParams.encode()
+	assert.Equal(t, 30, len(encoded)) // this will increase as more fields are supported
+}
+
+func TestUserParameterEncoding(t *testing.T) {
+	Setup()
+	emptyUserParams := &UserParameters{}
+	encoded := emptyUserParams.encode()
+	// the times should be set to the current server time automatically
+	assert.Equal(t, 3, len(encoded))
+	// populate all the fields and encode
+	encoded = testUserParams.encode()
+	assert.Equal(t, 24, len(encoded))
+
+	assert.Equal(t, fmt.Sprintf("%d", *testUserParams.IDTS), encoded["_idts"])
+	assert.Equal(t, fmt.Sprintf("%d", *testUserParams.ViewTS), encoded["_viewts"])
+	assert.Equal(t, "1", encoded["_idvc"])
+	assert.Equal(t, "Keyword+Test", encoded["_rck"])
+	assert.Equal(t, "Testing", encoded["_rcn"])
+	assert.Equal(t, "0", encoded["ag"])
+	assert.Equal(t, "1", encoded["cookie"])
+	assert.Equal(t, "1", encoded["dir"])
+	assert.Equal(t, "1", encoded["fla"])
+	assert.Equal(t, "1", encoded["gears"])
+	assert.Equal(t, "1", encoded["java"])
+	assert.Equal(t, "EO", encoded["lang"])
+	assert.Equal(t, "1", encoded["new_visit"])
+	assert.Equal(t, "1", encoded["pdf"])
+	assert.Equal(t, "1", encoded["qt"])
+	assert.Equal(t, "0", encoded["realp"])
+	assert.Equal(t, "1x1", encoded["res"])
+	assert.Equal(t, "test-user", encoded["uid"])
+	assert.Equal(t, "%2Fusers", encoded["urlref"])
+	assert.Equal(t, "1", encoded["wma"])
+
+}
+func TestEventParameterEncodings(t *testing.T) {
+	Setup()
+	emptyEventParams := &EventTrackingParameters{}
+	encoded := emptyEventParams.encode()
+	assert.Equal(t, 0, len(encoded))
+	// populate all the fields and encode
+	encoded = testEventParams.encode()
+	assert.Equal(t, url.QueryEscape(*testEventParams.Category), encoded["e_c"])
+	assert.Equal(t, url.QueryEscape(*testEventParams.Action), encoded["e_a"])
+	assert.Equal(t, url.QueryEscape(*testEventParams.Name), encoded["e_n"])
+	assert.Equal(t, url.QueryEscape(fmt.Sprintf("%v", *testEventParams.Value)), encoded["e_v"])
+}
+
+var allParams = Parameters{
+	RecommendedParameters:     &RecommendedParameters{},
+	UserParameters:            testUserParams,
+	ActionParameters:          &ActionParameters{},
+	PagePerformanceParameters: &PagePerformanceParameters{},
+	EventTrackingParameters:   testEventParams,
+	ContentTrackingParameters: &ContentTrackingParameters{},
+	EcommerceParameters:       &EcommerceParameters{},
+}
+
+var testUserParams = &UserParameters{
+	URLRef:           StringPtr("/users"),
+	CVar:             StringPtr("{\"id\": 1}"),
+	IDVC:             Int64Ptr(1),
+	ViewTS:           Int64Ptr(time.Now().Add(-1 * time.Minute).Unix()),
+	IDTS:             Int64Ptr(time.Now().Add(-10 * time.Minute).Unix()),
+	CampaignName:     StringPtr("Testing"),
+	CampaignKeyword:  StringPtr("Keyword Test"),
+	Resolution:       StringPtr("1x1"),
+	CookiesSupported: BoolPtr(true),
+	UserAgent:        StringPtr("ServerTest"),
+	Lang:             StringPtr("EO"),
+	UserID:           StringPtr("test-user"),
+	NewVisit:         BoolPtr(true),
+	UserPlugins: &UserPlugins{
+		Flash:       BoolPtr(true),
+		Java:        BoolPtr(true),
+		Director:    BoolPtr(true),
+		Quicktime:   BoolPtr(true),
+		RealPlayer:  BoolPtr(false),
+		PDF:         BoolPtr(true),
+		WMA:         BoolPtr(true),
+		Gears:       BoolPtr(true),
+		Silverlight: BoolPtr(false),
+	},
+}
+
+var testEventParams = &EventTrackingParameters{
+	Category: StringPtr("Event Category"),
+	Action:   StringPtr("Event Action"),
+	Name:     StringPtr("Event Name"),
+	Value:    Float64Ptr(42.42),
 }
